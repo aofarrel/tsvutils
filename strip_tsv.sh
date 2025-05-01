f3f187a4fc83d6944d31c1efc392e766d81fa5ca
Ash O'Farrell <aofarrel@ucsc.edu>
2025-05-01 15:21:53 -0700

    Upload the correct file
    Oops!

--- strip_tsv.sh
+++ strip_tsv.sh
@@ -1,29 +1,19 @@
 #!/bin/bash
 
 input_file="$1"
-temp_file="${input_file}.tmp"
+output_file="$2"
+temp_file="truncated"
 
-# Check if input file is provided
-if [[ -z "$input_file" ]]; then
-  echo "Usage: $0 <input_file>"
-  exit 1
-fi
+while IFS= read -r line; do
+    tab_count=$(grep -o $'\t' <<< "$line" | wc -l)
 
-# Step 1: Find the max number of tabs in any line
-max_tabs=$(awk -F'\t' '{if (NF-1 > max) max = NF-1} END {print max}' "$input_file")
+    if (( tab_count > 2 )); then
+        echo "$line" >> "$output_file"
+        truncated=$(cut -f1-3 <<< "$line")
+        echo -e "$truncated" >> "$temp_file"
+    else
+        echo "$line" >> "$temp_file"
+    fi
+done < "$input_file"
 
-# Step 2: Pad each line with extra tabs to reach max_tabs
-awk -v max="$max_tabs" -F'\t' '{
-    extra = max - (NF - 1)
-    printf "%s", $1
-    for (i = 2; i <= NF; i++) {
-        printf "\t%s", $i
-    }
-    for (i = 0; i < extra; i++) {
-        printf "\t"
-    }
-    printf "\n"
-}' "$input_file" > "$temp_file"
-
-# Step 3: Replace the original file
 mv "$temp_file" "$input_file"
