2bd7a3bad81a6b760df26e9405802b24f0a0afdb
Ash O'Farrell <aofarrel@ucsc.edu>
2025-05-06 17:35:04 -0700

    Create isolate_column.sh

---
+++ isolate_column.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#
+# Remove all columns except for one
+
+input_file="$1"
+column_number="$2"
+extension="${input_file##*.}"
+
+if [ -z "$1" ]; then
+  echo "Usage: $0 <csv_or_tsv_file> <column_number>"
+  echo "Column numbering is 1-indexed (eg, first column is 1)"
+  echo "Input file is expected to end in .tsv or .csv"
+  exit 1
+fi
+
+if [[ "$extension" == "tsv" ]]; then
+  awk -F '	' "{print \$$column_number}" "$1"
+elif [[ "$extension" == "csv" ]]; then
+  awk -F ',' "{print \$$column_number}" "$1"
+else
+  echo "Unknown file type"
+fi
+
